5eca961b31c23ab63922bd06a734200a3b7d25ba
Jesus Gonzales <jesus.gonzales1995@gmail.com>
2021-09-13 16:57:27 -0700

    mvp of my reviews layout

--- src/Components/Reviews/ReviewListEntry.jsx
+++ src/Components/Reviews/ReviewListEntry.jsx
@@ -1,16 +1,24 @@
 import React, { useState } from 'react';
 
-var ReviewListEntry = ({review, setReviews, formatDate}) => {
+var ReviewListEntry = ({ review, setReviews, formatDate }) => {
 
   return (
-    <div>
-      <div>Rating: {review.rating}</div>
-      <div><b>{review.summary}</b></div>
-      <div>{review.body}</div>
-      <div>{review.response}</div>
-      <div>{review.reviewer_name + ', ' + formatDate(review.date)}</div>
-      <div>Helpful? Yes ({review.helpfulness}) | Report </div>
-    </div>
+    (review.response) ?
+      < div id='reviewEntry' >
+        <div id='rating'>Rating: {review.rating}</div>
+        <div id='date'>{review.reviewer_name + '   ' + formatDate(review.date)}</div>
+        <div id='summary'><b>{review.summary}</b></div>
+        <div id='body'>{review.body}</div>
+        <div id='response'>Response: <br></br> <br></br>{review.response}</div>
+        <div id='helpful'>Helpful? <u>Yes({review.helpfulness})</u>  | <u>Report</u> </div>
+      </div > :
+      <div id='reviewEntry'>
+        <div id='rating'>Rating: {review.rating}</div>
+        <div id='date'>{review.reviewer_name + '   ' + formatDate(review.date)}</div>
+        <div id='summary'><b>{review.summary}</b></div>
+        <div id='body'>{review.body}</div>
+        <div id='helpful'>Helpful? <u>Yes({review.helpfulness})</u> | <u>Report</u> </div>
+      </div>
   )
 }
 
