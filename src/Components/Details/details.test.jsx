c831fc3a1b1b7078099478ba6000e09323985727
Lenora Esquenazi <lenaciousd@gmail.com>
2021-09-14 14:43:24 -0700

    Added some comments for installation/setup for tests to work

--- src/Components/Details/details.test.jsx
+++ src/Components/Details/details.test.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-import {render} from '@testing-library/react'
-import '@testing-library/jest-dom'
-import details from './Details.jsx'
+import React from 'react';
+import { render, fireEvent, waitFor, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import details from './Details.jsx';
+import jest from 'jest';
+// We need install msw and create a mock server for testing
+// import {rest} from 'msw';
+// import {setupServer} from 'msw/node';
 
-
-test('product description component loads', () => {
-  expect(details).toBe(true);
+describe('details', () => {
+  test('product description component loads', () => {
+    render(<Details />);
+    expect(details).toBe(true);
+  });
+  // test('handles server error', () => {
+  //   server.use(
+  //     rest.get('/greeting', (req, res, ctx) => {
+  //       return res(ctx.status(500))
+  //     }),
+  //   )
 });
