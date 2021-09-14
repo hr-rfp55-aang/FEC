2e44b0d4cadca73b07c47b75954373823b05cbe9
Lenora Esquenazi <lenaciousd@gmail.com>
2021-09-14 14:46:20 -0700

    Gets styles when product Info changes, uses condidtional rendering for the productstyles info.

--- src/Components/Details/Details.jsx
+++ src/Components/Details/Details.jsx
@@ -1,27 +1,27 @@
 import React, { useState, useEffect, useContext} from 'react';
-import ProductInfo from './ProductInfo.jsx';
+import ProductDescription from './ProductDescription.jsx';
 import { ContextObj } from '../../ContextObj.jsx';
 import './details.css';
 
 const Details = () => {
-  const [productStyles, setProductStyles] = useState({})
+  const [productStyles, setProductStyles] = useState({results: []});
 
-  const {productInfo, getServer} = useContext(ContextObj)
-  const id = productInfo.id
+  const {productInfo, getServer} = useContext(ContextObj);
+  const id = productInfo.id;
 
   useEffect(() => {
     getServer(`/products/${id}/styles`, (result) => setProductStyles(result));
-  }, []);
+  }, [productInfo]);
 
   return (
     <div>
       {/* Photo Gallery Component*/}
-      <ProductInfo productStyles={productStyles}/>
+      <ProductDescription productStyles={productStyles}/>
       {/* Style Selector Component*/}
       {/* Add to Cart Component*/}
       _______________________________________________
     </div>
-  )
-}
+  );
+};
 
 export default Details;
