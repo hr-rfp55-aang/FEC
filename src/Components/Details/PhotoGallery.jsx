277bd5dccb07b4179e78c83dfed069969e1a53aa
Lenora Esquenazi <lenaciousd@gmail.com>
2021-09-14 16:58:30 -0700

    Created a Photo Gallery Component

---
+++ src/Components/Details/PhotoGallery.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect, useContext} from 'react';
+import ProductDescription from './ProductDescription.jsx';
+// import PhotoGalleryListItem from './PhotoGalleryListItem';
+import { ContextObj } from '../../ContextObj.jsx';
+import './styles.css';
+
+const PhotoGallery = ({productStyles}) => {
+  console.log('In Photo Gallery ', productStyles);
+
+  // console.log('In photo gallery the first photo is ', productStyles.results[0].photos[0].url);
+
+  return (
+    <div>
+      Photo Gallery
+      <div>
+        {
+          productStyles.results.length > 0 &&
+          productStyles.results[0].photos.length > 0 &&
+          <img className="displayedPhoto" src={productStyles.results[0].photos[0].url} alt={productStyles.results[0].id}/>
+        }
+      </div>
+    </div>
+  );
+};
+export default PhotoGallery;
