1bd679f9fe695b9dbf6db4101c713d58f8fba89b
Lenora Esquenazi <lenaciousd@gmail.com>
2021-09-15 13:33:18 -0700

    Created a ThumbnailListItem component

---
+++ src/Components/Details/ThumbnailListItem.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect, useContext} from 'react';
+import ProductDescription from './ProductDescription.jsx';
+import { ContextObj } from '../../ContextObj.jsx';
+
+const ThumbnailListItem = ()=> {
+
+  return (
+    <div>I am one thumbnail</div>
+  );
+};
+
+export default ThumbnailListItem;
