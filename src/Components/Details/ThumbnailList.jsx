3f9abedec62a59c4f3d9f3448ef368c4ac2b7599
Lenora Esquenazi <lenaciousd@gmail.com>
2021-09-15 13:34:21 -0700

    Imported thumbnailistitem component

--- src/Components/Details/ThumbnailList.jsx
+++ src/Components/Details/ThumbnailList.jsx
@@ -1,12 +1,20 @@
 import React, { useState, useEffect, useContext} from 'react';
 import ProductDescription from './ProductDescription.jsx';
-// import ThumbnailListItem from './ThumbnailListItem';
+import ThumbnailListItem from './ThumbnailListItem';
 import { ContextObj } from '../../ContextObj.jsx';
 
-const ThumbnailList = () => {
+const ThumbnailList = ({productPhotos}) => {
+  console.log('In ThumbnailList ', productPhotos);
+
+  // const thumbnails = productPhotos.map((thumbnail, index) => {
+  //   <ThumbnailListItem key={index} img={thumbnail.thumbnail_url}/>;
+  // });
 
   return (
-    <div className="thumbnailList">ThumbnailList</div>
+    <div className="thumbnailList">
+    ThumbnailList
+      {/* {thumbnails} */}
+    </div>
   );
 };
 
