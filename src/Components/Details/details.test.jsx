fa24c038d99ada061ebdfc3388218d7aad139d09
Lenora Esquenazi <lenaciousd@gmail.com>
2021-09-15 11:27:07 -0700

    Changed test to find text in the Product escription component

--- src/Components/Details/details.test.jsx
+++ src/Components/Details/details.test.jsx
@@ -1,21 +1,46 @@
 import React from 'react';
 import { render, fireEvent, waitFor, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import details from './Details.jsx';
+import Details from './Details';
+import ProductDescription from './ProductDescription';
 import jest from 'jest';
-// We need install msw and create a mock server for testing
-// import {rest} from 'msw';
-// import {setupServer} from 'msw/node';
+import '@babel/plugin-syntax-jsx';
+import axios from 'axios';
 
-describe('details', () => {
-  test('product description component loads', () => {
-    render(<Details />);
-    expect(details).toBe(true);
-  });
-  // test('handles server error', () => {
-  //   server.use(
-  //     rest.get('/greeting', (req, res, ctx) => {
-  //       return res(ctx.status(500))
-  //     }),
-  //   )
+
+// Test for text appearing in a component:
+
+test('product description component loads', () => {
+  render(<ProductDescription/>);
+  const text = screen.getByText(/Read all 100 reviews/i);
+  expect(text).toBeInTheDocument();
 });
+
+// describe('Details', () => {
+//   it('should render a div', () => {
+//     render (<ProductDescription />);
+
+//     const text = screen.getByText(/Read all 100 reviews/i);
+
+//     expect(text).toBeInTheDocument();
+//   });
+// });
+
+
+// Test get request:
+
+// const server = 'http://localhost:3001';
+
+// test('get request', () => {
+//   axios.get(server + '/products/40350')
+//     .then((result) => {
+//       callback(result.data);
+//     })
+//     .catch((err) => {
+//       console.log('axios err', err);
+//     });
+
+//   const statusCode = result.statuscode;
+
+//   expect(statusCode).toBeInTheDocument;
+// });
