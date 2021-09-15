05ef40200dee9d605a5bcf36c741975f009dd715
Lenora Esquenazi <lenaciousd@gmail.com>
2021-09-15 11:58:38 -0700

    Created ThumbnailList component

---
+++ src/Components/Details/ThumbnailList.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useContext} from 'react';
+import ProductDescription from './ProductDescription.jsx';
+// import ThumbnailListItem from './ThumbnailListItem';
+import { ContextObj } from '../../ContextObj.jsx';
+
+const ThumbnailList = () => {
+
+  return (
+    <div className="thumbnailList">ThumbnailList</div>
+  );
+};
+
+export default ThumbnailList;
