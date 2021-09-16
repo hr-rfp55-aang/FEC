efe1ab82544ccc6d469b1bdc29bb28a0bee2c4e2
Lenora Esquenazi <lenaciousd@gmail.com>
2021-09-15 20:30:38 -0700

    Removed unnecessary props

--- src/Components/Details/ThumbnailList.jsx
+++ src/Components/Details/ThumbnailList.jsx
@@ -2,19 +2,15 @@ import React, { useState, useEffect, useContext } from 'react';
 import ProductDescription from './ProductDescription.jsx';
 import ThumbnailListItem from './ThumbnailListItem';
 import { ContextObj } from '../../ContextObj.jsx';
+import { getServer, grabReviewScore, formatDate } from '../../helpers';
 
-const ThumbnailList = ({ productPhotos, mainPhoto, setMainPhoto, mainPhotoName, setMainPhotoName }) => {
+const ThumbnailList = ({ productPhotos, mainPhoto, setMainPhoto, mainPhotoName }) => {
   //console.log('In ThumbnailList ', productPhotos);
-
-  const defaultThumbnail = 'https://images.unsplash.com/photo-1561861422-a549073e547a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80'
-  const [mainThumbnail, setMainThumbnail] = useState(
-    productPhotos.length > 0 ? productPhotos[0].thumbnail_url : defaultThumbnail
-  )
 
   return (
     <div className="thumbnailList">
-      {(productPhotos || []).map((photo, index) => {
-        return <ThumbnailListItem key={index} index={index.toString()} img={photo.thumbnail_url} photo={photo.url} mainPhoto={setMainPhoto} setMainPhoto={setMainPhoto} mainThumbnail={mainThumbnail} setMainThumbnail={setMainThumbnail} mainPhotoName={mainPhotoName} setMainPhotoName={setMainPhotoName}/>
+      {productPhotos.map((photo, index) => {
+        return <ThumbnailListItem key={index} index={index.toString()} photo={photo} mainPhoto={mainPhoto} setMainPhoto={setMainPhoto} mainPhotoName={mainPhotoName} />
       })}
     </div>
   );
--- src/Components/Details/ThumbnailListItem.jsx
+++ src/Components/Details/ThumbnailListItem.jsx
@@ -1,12 +1,13 @@
-import React, { useState, useEffect, useContext} from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import ProductDescription from './ProductDescription.jsx';
 import { ContextObj } from '../../ContextObj.jsx';
+import { getServer, grabReviewScore, formatDate } from '../../helpers';
 
-const ThumbnailListItem = ({img, index, photo, mainPhoto, setMainPhoto, mainThumbnail, setMainThumbnail,  mainPhotoName, setMainPhotoName})=> {
-    //console.log('In thumbnaillistlitem: ', photo)
+const ThumbnailListItem = ({ index, photo, mainPhoto, setMainPhoto, mainPhotoName }) => {
+  //console.log('In thumbnaillistlitem: ', photo)
   return (
     <div>
-      <img onClick={() => { setMainPhoto(photo), setMainThumbnail(img), setMainPhotoName()}}className="thumbnailListItem" src={img} alt={mainPhotoName + index}/>
+      <img onClick={() => { setMainPhoto(photo) }} className="thumbnailListItem" src={photo.thumbnail_url} alt={mainPhotoName + index} />
     </div>
   );
 };
