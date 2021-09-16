a4dec0442ca682fdd8659d868b7dbb25b9041de3
Lenora Esquenazi <lenaciousd@gmail.com>
2021-09-16 08:53:09 -0700

    uncommented Details component

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ const App = () => {
   return (
     <div>
       {isLoaded && <ContextObj.Provider value={{ productId, productInfo, ratingAvg, reviewsTotal }}>
-        {/* <Details /> */}
+        <Details />
         <Related />
         <Questions />
         <Reviews />
