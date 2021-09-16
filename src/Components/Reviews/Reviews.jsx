8ac9be79c1dc78c7a6773a6311a6fe023dfe7b86
Jesus Gonzales <jesus.gonzales1995@gmail.com>
2021-09-16 12:07:32 -0700

    fixed bugs with helpful, and added report functionality

--- src/Components/Reviews/Reviews.jsx
+++ src/Components/Reviews/Reviews.jsx
@@ -10,35 +10,51 @@ var Review = () => {
   const [reviewsLimit, setLimit] = useState(2);
   const [sortStr, setsortStr] = useState('');
   const [curReview, setCurReview] = useState('');
+  const [report, setReport] = useState('');
   const { productInfo, productId } = useContext(ContextObj);
 
 
   useEffect(() => {
-    getServer(`/reviews/?product_id=${productId}`)
+    getServer(`/reviews/?product_id=${productId}&count=100`)
       .then((result) => { setReviews(result.results); });
   }, [productInfo]);
 
   useEffect(() => {
-    getServer(`/reviews/?product_id=${productId}&sort=${sortStr}`)
+    getServer(`/reviews/?product_id=${productId}&sort=${sortStr}&count=100`)
       .then((result) => { setReviews(result.results); });
   }, [sortStr]);
 
   useEffect(() => {
-    if (curReview !== '') {
+    if (curReview !== '' && sortStr !== '') {
       putServer(`/reviews/${curReview}/helpful`)
-        .then(()=>getServer(`/reviews/?product_id=${productId}&sort=${sortStr}`)
-          .then((result) => { setReviews(result.results); }));
+        .then(() => getServer(`/reviews/?product_id=${productId}&sort=${sortStr}&count=100`))
+        .then((result) => { setReviews(result.results); });
       console.log('put');
+    } else if (curReview !== '') {
+      putServer(`/reviews/${curReview}/helpful`)
+        .then(() => getServer(`/reviews/?product_id=${productId}&count=100`))
+        .then((result) => { setReviews(result.results); });
     }
   }, [curReview]);
 
+  useEffect(() => {
+    if (report !== '' && sortStr !== '') {
+      putServer(`/reviews/${report}/report`)
+        .then(() => getServer(`/reviews/?product_id=${productId}&sort=${sortStr}&count=100`))
+        .then((result) => { setReviews(result.results); });
+    } else if (report !== '') {
+      putServer(`/reviews/${report}/report`)
+        .then(() => getServer(`/reviews/?product_id=${productId}&count=100`))
+        .then((result) => { setReviews(result.results); });
+    }
+  }, [report]);
 
   return (
     <div>
       <h4>Ratings & Reviews</h4>
       <div className='reviews'>
         <ReviewBreakdown reviews={reviews} />
-        <ReviewList reviews={reviews} setReviews={setReviews} setLimit={setLimit} reviewsLimit={reviewsLimit} setsortStr={setsortStr} setCurReview={setCurReview}/>
+        <ReviewList reviews={reviews} setReviews={setReviews} setLimit={setLimit} reviewsLimit={reviewsLimit} setsortStr={setsortStr} setCurReview={setCurReview} setReport={setReport} />
       </div>
     </div>
   );
