61c8180e9f9b78833e717afae54eaed1ff56d04e
Jesus Gonzales <jesus.gonzales1995@gmail.com>
2021-09-16 17:53:48 -0700

    Added dynamic star functionality

--- src/Components/Reviews/RatingBar.jsx
+++ src/Components/Reviews/RatingBar.jsx
@@ -2,50 +2,30 @@ import React from 'react';
 
 var RatingBar = () => {
   return (
-    <div>
-      <svg viewBox="0 0 1000 200" >
-        <defs>
 
-          <polygon id="outline" points="75,0 131,66 200,76 150,128 162,200 100,166 38,200 50,128 0,76 69,66 " />
 
-          {/* <clipPath id="stars">
-            <use xlinkHref="#star" />
-            <use xlinkHref="#star" x="20%" />
-            <use xlinkHref="#star" x="40%" />
-            <use xlinkHref="#star" x="60%" />
-            <use xlinkHref="#star" x="80%" />
-          </clipPath> */}
+    <svg viewBox="0 0 1000 200" className='rating'>
+      <defs>
 
-        </defs>
+        <polygon id="star" points="100,0 131,66 200,76 150,128 162,200 100,166 38,200 50,128 0,76 69,66 " />
 
-        {/* <rect className='rating__background' clip-path="url(#stars)"></rect>
+        <clipPath id="stars">
+          <use xlinkHref="#star" />
+          <use xlinkHref="#star" x="20%" />
+          <use xlinkHref="#star" x="40%" />
+          <use xlinkHref="#star" x="60%" />
+          <use xlinkHref="#star" x="80%" />
+        </clipPath>
 
-        <rect width="25%" className='rating__value' clip-path="url(#stars)"></rect> */}
+      </defs>
 
-      </svg>
+      <rect className='rating__background' clipPath="url(#bar)"></rect>
 
-      <svg viewBox="0 0 10 2" className='rating'>
-        <defs>
+      <rect width="88%" className='rating__value' clipPath="url(#bar)"></rect>
 
-          <polygon id="star" points="100,0 131,66 200,76 150,128 162,200 100,166 38,200 50,128 0,76 69,66 " />
+    </svg>
 
-          <clipPath id="stars">
-            <use xlinkHref="#star" />
-            <use xlinkHref="#star" x="20%" />
-            <use xlinkHref="#star" x="40%" />
-            <use xlinkHref="#star" x="60%" />
-            <use xlinkHref="#star" x="80%" />
-          </clipPath>
 
-        </defs>
-
-        <rect className='rating__background' clipPath="url(#outline)"></rect>
-
-        <rect width="20%" className='rating__value' clipPath="url(#bar)"></rect>
-
-      </svg>
-
-    </div>
   );
 };
 
