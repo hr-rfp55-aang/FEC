59ea38fe109a3515a487c3e77a01ff1b2987fc69
Matthew Boyle <matthewtboyle89@gmail.com>
2021-09-17 09:15:17 -0700

    added more questions button

--- src/Components/Questions/QAList.jsx
+++ src/Components/Questions/QAList.jsx
@@ -1,11 +1,17 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import QAListEntry from './QAListEntry.jsx';
 
 const QAList = (props) => {
+  // set a state of the questions limit starting at 4
+  // everytime the button gets clicked, increase that state by 2
+  // slice the questions list from 0 to
+
+  var questionList = props.questions.results.slice();
 
   return (
     <div>
       {props.questions.results.map((question, index) => (<QAListEntry question={question} key={index} />))}
+      <button>More Answered Questions</button>
     </div>
   );
 };
