b2e59d59fec721b30bd33a374f566a1f8d57fe23
Lenora Esquenazi <lenaciousd@gmail.com>
2021-09-16 20:35:30 -0700

    Removed unnecessary imports

--- src/Components/Details/ProductDescription.jsx
+++ src/Components/Details/ProductDescription.jsx
@@ -1,29 +1,23 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useContext } from 'react';
 import { ContextObj } from '../../ContextObj.jsx';
 
-const ProductDescription = ({ productStyles }) => {
+const ProductDescription = ({ currentProductStyle }) => {
 
-  const { productId, productInfo, ratingAvg, reviewsTotal } = useContext(ContextObj);
+  const { productInfo, ratingAvg, reviewsTotal } = useContext(ContextObj);
 
-  // const price = productStyles[0].original_price
+  const price = currentProductStyle.sale_price ? currentProductStyle.sale_price + currentProductStyle.original_price : currentProductStyle.original_price;
 
-  //console.log('In ProductInfo ', 'product: ', productInfo, 'productStyles: ', productStyles);
+  // console.log('In ProductDescription ', 'product: ', productInfo, 'currentProductStyle ', currentProductStyle);
 
   return (
     <div className="descriptionOverview">
-      {/* Star Rating Component */}
-      <div> {ratingAvg ? ratingAvg + `*** Read all ${reviewsTotal} reviews` : 'No Ratings Available for this Product'}  </div>
-      {/* Product Category */}
+      {/* Star Rating */}
+      <div> {ratingAvg ? ratingAvg + `*** Read all ${reviewsTotal} reviews` : 'No Ratings Available for this Product'} </div>
       <div>{productInfo.category}</div>
       {/* Product Name */}
       <div><h2>{productInfo.name}</h2></div>
-      {/* Price Component */}
-      <div>
-        ${productStyles.length > 0 &&
-          productStyles[0].original_price
-        }
-      </div>
-      {/* Product Description Component*/}
+      <div>${price} </div>
+      {/* Product Description */}
       <div className="detailedDescription">{productInfo.description}</div>
       {/* Share on Social Media Component */}
     </div>
