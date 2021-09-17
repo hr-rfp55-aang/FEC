9cac58cdc88813935e96e2db172182c69930dbab
Lenora Esquenazi <lenaciousd@gmail.com>
2021-09-16 20:38:08 -0700

    Simplified Solution and removed unnecessary, state, props, and imports

--- src/Components/Details/SizeOptions.jsx
+++ src/Components/Details/SizeOptions.jsx
@@ -1,12 +1,8 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React from 'react';
 
-const SizeOptions = ({sku, size, currentSize, setCurrentSize}) => {
-
-  return (
-    <option value={size.size}>
-      {size.size}
-    </option>);
-
-};
+const SizeOptions = ({ sku, size }) =>
+  <option name={size.size} value={sku}>
+    {size.size}
+  </option>;
 
 export default SizeOptions;
