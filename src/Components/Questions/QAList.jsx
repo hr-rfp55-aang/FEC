5334c40ddac85007c60bc5e8598fd050a126e7d6
Matthew Boyle <matthewtboyle89@gmail.com>
2021-09-17 14:11:56 -0700

    if there are no more questions, button disappears

--- src/Components/Questions/QAList.jsx
+++ src/Components/Questions/QAList.jsx
@@ -3,9 +3,6 @@ import QAListEntry from './QAListEntry.jsx';
 import {ContextObj} from '../../ContextObj';
 
 const QAList = (props) => {
-  // set a state of the questions limit starting at 4
-  // everytime the button gets clicked, increase that state by 2
-  // slice the questions list from 0 to
   var questionList = props.questions.results;
   const [limit, setLimit] = useState(4);
   const {productId} = useContext(ContextObj);
@@ -26,6 +23,15 @@ const QAList = (props) => {
   if (questionList.length === 0) {
     return null;
   }
+
+  if (questionList.length < 3) {
+    return (
+      <div>
+        {listHandler(questionList).map((question, index) => (<QAListEntry question={question} key={index} />))}
+      </div>
+    );
+  }
+
   return (
     <div>
       {listHandler(questionList).map((question, index) => (<QAListEntry question={question} key={index} />))}
