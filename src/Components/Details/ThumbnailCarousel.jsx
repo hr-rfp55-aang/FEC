90d397261c9d7b41a0b5eb63a2486bdf6feea564
Lenora Esquenazi <lenaciousd@gmail.com>
2021-09-18 21:58:25 -0700

    Adeed some functionality

--- src/Components/Details/ThumbnailCarousel.jsx
+++ src/Components/Details/ThumbnailCarousel.jsx
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles.css';
 
 const ThumbnailCarousel = (props) => {
-  const {children} = props;
+  const { children } = props;
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [length, setLength] = useState(children.length);
+
+  const next = () => {
+    if (currentIndex < (length - 1)) {
+      setCurrentIndex(prevState => prevState + 1);
+    }
+  };
+
+  const prev = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(prevState => prevState - 1);
+    }
+  };
+
+  // Set the length to match current children from props
+  useEffect(() => {
+    setLength(children.length);
+  }, [children]);
 
   return (
-    <div className="carousel-container">
-      <div className="carousel-wrapper">
-        <div className="carousel-content-wrapper">
-          <div className="carousel-content">
+    <div className="ThumbnailCarousel-container">
+      <div className="ThumbnailCarousel-wrapper">
+        {currentIndex > 0 &&
+          <button onClick={prev} className="up-arrow">
+            &uarr;
+          </button>
+        }
+        <div className="ThumbnailCarousel-content-wrapper">
+          <div id="ThumbnailCarousel-content" className="ThumbnailCarousel-content" style={{ transform: `translateY(-${currentIndex * 100}%)` }}>
             {children}
           </div>
         </div>
+        {currentIndex < (length - 1) &&
+          <button onClick={next} className="down-arrow">
+            &darr;
+          </button>
+        }
       </div>
     </div>
   );
