b198623d32217b05088939effc7d57a163e6cfd3
Lenora Esquenazi <lenaciousd@gmail.com>
2021-09-22 12:35:58 -0700

    Changed name to be consistent

--- src/Components/Details/MainPhotoModal.jsx
+++ src/Components/Details/MainPhotoModal.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const MainImageModal = ({ mainPhoto, closeMainPhotoModal, enlargeMainPhoto, productPhotos, currentProductStyle, setMainPhoto, mainPhotoName, index, photos, getNextImage, getPreviousImage}) => {
+const MainPhotoModal = ({ mainPhoto, closeMainPhotoModal, enlargeMainPhoto, productPhotos, currentProductStyle, setMainPhoto, mainPhotoName, index, photos, getNextImage, getPreviousImage}) => {
 
   const showOrHide = enlargeMainPhoto ? 'modal display-block' : 'modal display-none';
 
   const modalThumbnails = productPhotos.map((photo, index) => {
-    return <img onClick={() => { setMainPhoto(photo); }}
+    return <img onClick={() => { setMainPhoto(photo); }} key={index}
       className={mainPhoto.url === photo.url ? 'productMainPhotoThumbnail thumbnailListItem' : 'thumbnailListItem'} src={photo.thumbnail_url} alt={mainPhotoName + index} />;
   });
 
@@ -46,4 +46,4 @@ const MainImageModal = ({ mainPhoto, closeMainPhotoModal, enlargeMainPhoto, prod
   );
 };
 
-export default MainImageModal;
+export default MainPhotoModal;
