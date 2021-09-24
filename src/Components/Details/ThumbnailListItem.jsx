2e7d1e867ee34ab62f75a60dc118c778e2a3b58e
Lenora Esquenazi <lenaciousd@gmail.com>
2021-09-24 11:49:22 -0700

    Added a default image when one doesn't exist

--- src/Components/Details/ThumbnailListItem.jsx
+++ src/Components/Details/ThumbnailListItem.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
 import ProductDescription from './ProductDescription.jsx';
+import missingImg from '../../assets/pants.svg';
 
 const ThumbnailListItem = ({ index, photo, mainPhoto, setMainPhoto, mainPhotoName }) => {
   // console.log('In thumbnaillistlitem: ', mainPhoto)
@@ -8,7 +9,7 @@ const ThumbnailListItem = ({ index, photo, mainPhoto, setMainPhoto, mainPhotoNam
       <img onClick={() => { setMainPhoto(photo); }}
         className={mainPhoto.url === photo.url ? 'productMainPhotoThumbnail thumbnailListItem' :
           'thumbnailListItem'}
-        src={photo.thumbnail_url} alt={mainPhotoName + index} />
+        src={photo.thumbnail_url || missingImg} alt={mainPhotoName + index} />
     </div>
   );
 };
